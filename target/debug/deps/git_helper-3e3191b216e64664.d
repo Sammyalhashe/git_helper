20265595f9cd05fe7fb216e82441dd5a2258c62a
Sammy Al Hashemi <sammy.alhashemi@mail.utoronto.ca>
2023-01-27 00:08:05 -0500

    Cleaning up.

--- target/debug/deps/git_helper-3e3191b216e64664.d
+++
@@ -1,5 +0,0 @@
-/home/sammyalhashemi/Documents/personal/ws/git_helper/target/debug/deps/git_helper-3e3191b216e64664.rmeta: src/lib.rs
-
-/home/sammyalhashemi/Documents/personal/ws/git_helper/target/debug/deps/git_helper-3e3191b216e64664.d: src/lib.rs
-
-src/lib.rs:
